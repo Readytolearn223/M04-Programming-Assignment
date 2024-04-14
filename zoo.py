f3d31e59a4b2748883e87b916cfc6b3b81597e60
Michael <Mb31914@hotmail.com>
2024-04-14 15:59:23 -0400

    Completed  MO4 Programming Assignment

---
+++ zoo.py
@@ -0,0 +1,18 @@
+def hours():
+  print('Open 9-5 daily') 
+
+"""
+ import zoo 
+zoo.hours()
+Open 9-5 daily
+ import zoo as menagerie
+ menagerie.hours()
+Open 9-5 daily
+ from zoo import hours
+ hours()
+Open 9-5 daily
+ from zoo import hours as info
+ info()
+Open 9-5 daily
+ exit()
+"""
